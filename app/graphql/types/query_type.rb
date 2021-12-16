ae0085bf21f402742e0ba6a5bc069f50cab1fee7
Nimesh Nikum <nimesh.nikum@pairfinance.com>
2021-12-16 22:07:56 +0100

    initial commit

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,12 @@
+module Types
+  class QueryType < Types::BaseObject
+    # queries are just represented as fields
+    # `all_links` is automatically camelcased to `allLinks`
+    field :all_links, [LinkType], null: false
+
+    # this method is invoked, when `all_link` fields is being resolved
+    def all_links
+      Link.all
+    end
+  end
+end
